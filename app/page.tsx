bce753bf5822ce2e2576763a3f4163d6f6d4a2ea
Greybillions <olayile1999@gmail.com>
2025-01-07 08:54:02 +0100

    implemented darkmode

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+'use client';
+
+import ThemeSwitch from '@/components/ThemeSwitch';
+
 const page = () => {
   return (
-    <div>
-      <h1>Qareer</h1>
+    <div className='text-primary-light dark:text-primary-dark flex flex-col relative items-center justify-center h-screen bg-n-light dark:bg-n-dark'>
+      <h1 className='text-4xl'>Qareer</h1>
+      <ThemeSwitch />
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          light: '#1e90ff', // Dark text for light theme
+          dark: '#1e90ff', // Light text for dark theme
+        },
+        n: {
+          light: '#f5f5f5', // Background for light theme
+          dark: '#444', // Background for dark theme
+        },
       },
     },
   },
